38d428f963b4c93f14014fb1736077dd41d7da50
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-08-11 14:09:24 -0400

    build signup rout/ signup form basics

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,37 @@
-class UserController < ApplicationController  
+class UserController < ApplicationController   
 
-   
+
+
+    get "/" do
+        erb :"users/home"
+    end
+
+   get "/users/signup" do
+     erb :"/users/signup" 
+   end 
+
+   post "/users" do  
+    #binding.pry 
+    @user = User.new(username: params[:username], 
+                        email: params[:email], 
+                     password: params[:password]) 
+     @user.save  
+
+     redirect "/users/home"
+   end 
+
+
+
+
+
+
+
+
+
+   get "/users/home" do 
+   # @user = User.find(session[:user_id]) 
+
+    erb :"/users/home" 
+   end
  
 end

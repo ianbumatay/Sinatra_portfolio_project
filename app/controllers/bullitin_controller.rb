cf8a68ff4b8422c318d87b520244b13bf274e451
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-08-11 01:43:49 -0400

    build Create and Read for activerecord CRUD

--- app/controllers/bullitin_controller.rb
+++ app/controllers/bullitin_controller.rb
@@ -4,6 +4,24 @@ class BullitinController <ApplicationController
         @bullitins = Bullitin.all 
 
         erb :"/bullitins/index" 
+    end 
+
+    get "/bullitins/new" do 
+        erb :"/bullitins/new" 
+    end 
+
+    post "/bullitins" do  
+        #binding.pry
+       @bullitin = Bullitin.create(title: params[:title], content: params[:content])
+
+       redirect "/bullitins/#{@bullitin.id}"
+    end 
+
+    get "/bullitins/:id" do  
+        #binding.pry
+        @bullitin = Bullitin.find(params[:id]) 
+
+        erb :"/bullitins/show" 
     end
 
 end

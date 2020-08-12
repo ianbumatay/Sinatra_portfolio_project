493ac29a0f5b1e8736d178384a705aac432d8c63
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-08-12 18:05:25 -0400

    updated users/login and set authenticate and session

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,6 @@ class UserController < ApplicationController
     end
 
     get "/users/signup" do
-
         erb :"/users/signup"
     end 
 
@@ -15,7 +14,22 @@ class UserController < ApplicationController
         user = User.create(params)  
       
         redirect :"/bullitins"
+     end  
+
+
+     get "/users/login" do 
+        erb :"/users/login" 
      end
+
+     post "/login" do 
+        @user = User.find_by(username: params[:username]) 
+        if @user && @user.authenticate(params[:password])
+            session[user_id] = @user.id
+            redirect "/bullitins" 
+        else 
+            redirect "/users/login" 
+        end 
+    end
 
    
 
